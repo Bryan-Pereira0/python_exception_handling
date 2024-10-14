9f6d52fc482014fe791d3bad124dbee2ab389508
bryanpereira <gfxmagicz@gmail.com>
2024-10-14 00:09:22 -0400

    added seperate function to weather conversion

--- exceptional_weather_forecast.py
+++ exceptional_weather_forecast.py
@@ -1,15 +1,18 @@
 #task1234
 
 
-def temp_conversion():
-    try:
-        temperature_in_f = float(input("What is the temperature in Fahrenheit?: "))
-        temperature_in_c = (temperature_in_f - 32) * 5/9
-    except ValueError:
-        print("That's not a valid number! Please run the app again and input a valid number.")
-    else:
-        print(f"{temperature_in_f} degrees Fahrenheit is {temperature_in_c} degrees Celsius!")
-    finally:
-        print("Thank you for using my Temperature Conversion App!")
+def temp_conversion(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
 
-temp_conversion()
+def weather_app():
+    try:
+        fahrenheit = float(input("What is the temperature in Fahrenheit?: "))
+        celsius = temp_conversion(fahrenheit)
+        
+    except ValueError:
+        print("Invalid number. Please run the app again and enter a valid number!")
+    else:
+        print(f"{fahrenheit} degrees Fahrenheit is {celsius} degrees Celsius.")
+    finally:
+        print("Thank you for using my weather application!")
+weather_app()
